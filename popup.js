e49ec0457142d9373573d13c324b899c1a33777e
dev-coco <63853884+dev-coco@users.noreply.github.com>
2020-12-03 16:47:25 -0400

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var button = document.getElementById('scrollup');
-    button.addEventListener('click', function() {
-        scroll(0,0);
-    });
-});
